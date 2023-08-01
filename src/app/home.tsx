80aa0dca44cfb7ee7e59c666c6e8cb382d95e886
CircuitSacul <circuitsacul@icloud.com>
2023-07-31 21:15:05 -0400

    implement searching by conference title (#36)

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,4 +1,5 @@
-import { useContext, useLayoutEffect, useMemo, useState } from "react";
+import Fuse from "fuse.js";
+import { useContext, useEffect, useMemo, useState } from "react";
 import { FlatList } from "react-native";
 import { List, useTheme } from "react-native-paper";
 
@@ -15,23 +16,48 @@ type Props = NativeStackScreenProps<RootStackParamList, "Home">;
 
 export default function Home({ navigation }: Props) {
   const theme = useTheme();
-  const conferences = useContext(ConferencesContext);
+
   const refreshConferences = useContext(RefreshConferencexContext);
   const [refreshing, setRefreshing] = useState(false);
 
+  const conferences = useContext(ConferencesContext);
+  const fuse = useMemo(
+    () => new Fuse(conferences, { keys: ["data.title"] }),
+    [conferences],
+  );
+
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerSearchBarOptions: {
+        onChangeText: (text) => setSearch(text.nativeEvent.text),
+      },
+    });
+  }, [navigation]);
+
   const listItems = useMemo(() => {
-    let items = [
-      <List.Item
-        titleStyle={{ color: theme.colors.primary }}
-        title="Add Conference"
-        onPress={() => navigation.navigate("NewConference")}
-        left={({ style }) => (
-          <List.Icon icon="plus" color={theme.colors.primary} style={style} />
-        )}
-      />,
-    ];
+    let items = [];
+    let toPush;
+
+    if (search === "") {
+      items.push(
+        <List.Item
+          titleStyle={{ color: theme.colors.primary }}
+          title="Add Conference"
+          onPress={() => navigation.navigate("NewConference")}
+          left={({ style }) => (
+            <List.Icon icon="plus" color={theme.colors.primary} style={style} />
+          )}
+        />,
+      );
+      toPush = conferences;
+    } else {
+      toPush = fuse.search(search).map((item) => item.item);
+    }
+
     items.push(
-      ...conferences.map((item) => {
+      ...toPush.map((item) => {
         return (
           <List.Item
             title={item.data.title}
@@ -42,16 +68,9 @@ export default function Home({ navigation }: Props) {
         );
       }),
     );
-    return items;
-  }, [conferences]);
 
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerSearchBarOptions: {
-        // search bar options
-      },
-    });
-  }, [navigation]);
+    return items;
+  }, [conferences, search, fuse]);
 
   return (
     <FlatList
