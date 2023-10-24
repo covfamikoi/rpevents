447aec9c9bd70f8ee7de286f4a62c0c27b7ce8d2
CircuitSacul <circuitsacul@icloud.com>
2023-10-23 20:48:24 -0400

    user-facing announcements view (#44)

--- src/app/conference/announcements.tsx
+++ src/app/conference/announcements.tsx
@@ -1,7 +1,11 @@
-import { ScrollView } from "react-native";
-import { Text } from "react-native-paper";
+import { useEffect, useMemo } from "react";
+import { FlatList } from "react-native";
+import { List, Text } from "react-native-paper";
 
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+
+import { useConferenceStream } from "../../contexts/conferenceStream";
+import { Announcement } from "../../models";
 
 import { RootStackParamList } from "..";
 
@@ -10,10 +14,42 @@ type Props = NativeStackScreenProps<
   "ViewConferenceAnnouncements"
 >;
 
-export default function ViewConferenceAnnouncements({}: Props) {
+export default function ViewConferenceAnnouncements({
+  route,
+  navigation,
+}: Props) {
+  const conference = useConferenceStream(route.params.conference, () =>
+    navigation.goBack(),
+  );
+  const announcements = useMemo(() => {
+    return conference.data.announcements.sort(
+      (a, b) => b.postedAt.toMillis() - a.postedAt.toMillis(),
+    );
+  }, [conference.data.announcements]);
+
   return (
-    <ScrollView contentInsetAdjustmentBehavior="automatic">
-      <Text>Announcements</Text>
-    </ScrollView>
+    <FlatList
+      contentInsetAdjustmentBehavior="automatic"
+      data={announcements}
+      renderItem={(item) => <AnnouncementView item={item.item} />}
+    />
+  );
+}
+
+function AnnouncementView({ item }: { item: Announcement }) {
+  return (
+    <List.Item
+      title={item.title}
+      description={item.content}
+      descriptionNumberOfLines={3}
+      right={() => (
+        <Text>
+          {item.postedAt.toDate().toLocaleString(undefined, {
+            dateStyle: "medium",
+            timeStyle: "short",
+          })}
+        </Text>
+      )}
+    />
   );
 }
